552b5ae6622d416855623f2bfd73e8cdf4c156df
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-20 23:30:35 +0000

    Add utility to merge multiple JSON files into a single JSON object

---
+++ ethportal-peertest/src/scenarios/json_merger.rs
@@ -0,0 +1,23 @@
+use serde_json::{Map, Value};
+use std::fs;
+use std::path::Path;
+
+pub fn merge_json_files(file_paths: &[&str]) -> Result<Value, Box<dyn std::error::Error>> {
+    let mut merged_map = Map::new();
+
+    for path_str in file_paths {
+        let path = Path::new(path_str);
+        let content = fs::read_to_string(path)?;
+        let json_value: Value = serde_json::from_str(&content)?;
+
+        if let Value::Object(map) = json_value {
+            for (key, value) in map {
+                merged_map.insert(key, value);
+            }
+        } else {
+            return Err("Each JSON file must contain a JSON object".into());
+        }
+    }
+
+    Ok(Value::Object(merged_map))
+}
