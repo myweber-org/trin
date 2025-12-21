33bd7d371137885017586566afbaccd7a90423db
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-21 20:27:09 +0000

    Implement a basic WebSocket echo server using tokio-tungstenite

---
+++ test_assets/beacon/bellatrix/LightClientUpdate/ssz_random/case_2/websocket_server.rs
@@ -0,0 +1,32 @@
+use tokio::net::TcpListener;
+use tokio_tungstenite::accept_async;
+use futures_util::{SinkExt, StreamExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await?;
+    println!("WebSocket echo server listening on {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(stream).await {
+                eprintln!("Error handling connection: {}", e);
+            }
+        });
+    }
+    Ok(())
+}
+
+async fn handle_connection(stream: tokio::net::TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    let ws_stream = accept_async(stream).await?;
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(msg) = read.next().await {
+        let msg = msg?;
+        if msg.is_text() || msg.is_binary() {
+            write.send(msg).await?;
+        }
+    }
+    Ok(())
+}
