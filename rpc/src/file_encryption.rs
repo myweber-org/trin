cffca17c451d2f2eaf4a680ed8eda78593cfe89a
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-25 18:21:35 +0000

    Add file encryption utility with AES-256-GCM

---
+++ rpc/src/file_encryption.rs
@@ -0,0 +1,25 @@
+use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce,
+};
+use std::error::Error;
+
+pub fn encrypt_file(key: &[u8; 32], plaintext: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key));
+    let nonce = Nonce::from_slice(b"unique_nonce_12");
+    let ciphertext = cipher.encrypt(nonce, plaintext)?;
+    Ok(ciphertext)
+}
+
+pub fn decrypt_file(key: &[u8; 32], ciphertext: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key));
+    let nonce = Nonce::from_slice(b"unique_nonce_12");
+    let plaintext = cipher.decrypt(nonce, ciphertext)?;
+    Ok(plaintext)
+}
+
+pub fn generate_key() -> [u8; 32] {
+    let mut key = [0u8; 32];
+    OsRng.fill_bytes(&mut key);
+    key
+}
