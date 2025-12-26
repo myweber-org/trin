f809e8c437b732ee764e93d2456a7a7e8c4889ca
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-26 20:45:58 +0000

    Add file encryption utility with AES-256-GCM

--- test_assets/beacon/capella/LightClientOptimisticUpdate/file_encryption.rs
+++ test_assets/beacon/capella/LightClientOptimisticUpdate/file_encryption.rs
@@ -110,4 +110,46 @@ mod tests {
         
         Ok(())
     }
+}use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce,
+};
+use std::error::Error;
+
+pub fn encrypt_data(plaintext: &[u8]) -> Result<(Vec<u8>, Vec<u8>), Box<dyn Error>> {
+    let key = Aes256Gcm::generate_key(&mut OsRng);
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    
+    let ciphertext = cipher.encrypt(&nonce, plaintext)?;
+    Ok((ciphertext, nonce.to_vec()))
+}
+
+pub fn decrypt_data(ciphertext: &[u8], nonce: &[u8], key: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let key = Key::<Aes256Gcm>::from_slice(key);
+    let cipher = Aes256Gcm::new(key);
+    let nonce = Nonce::from_slice(nonce);
+    
+    let plaintext = cipher.decrypt(nonce, ciphertext)?;
+    Ok(plaintext)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encryption_roundtrip() {
+        let original_data = b"Secret message for encryption test";
+        let (ciphertext, nonce) = encrypt_data(original_data).unwrap();
+        
+        let key = Aes256Gcm::generate_key(&mut OsRng).to_vec();
+        let cipher = Aes256Gcm::new(&Key::<Aes256Gcm>::from_slice(&key));
+        let nonce_obj = Nonce::from_slice(&nonce);
+        
+        let encrypted = cipher.encrypt(nonce_obj, original_data).unwrap();
+        let decrypted = cipher.decrypt(nonce_obj, &encrypted).unwrap();
+        
+        assert_eq!(original_data.to_vec(), decrypted);
+    }
 }
