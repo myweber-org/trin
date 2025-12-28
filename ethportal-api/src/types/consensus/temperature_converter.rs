eba2992d9f66288da561399732fc9576a1f3f869
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-28 16:05:43 +0000

    Add a simple Celsius to Fahrenheit converter function

---
+++ ethportal-api/src/types/consensus/temperature_converter.rs
@@ -0,0 +1,10 @@
+
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+    (celsius * 9.0 / 5.0) + 32.0
+}
+
+fn main() {
+    let celsius_temp = 25.0;
+    let fahrenheit_temp = celsius_to_fahrenheit(celsius_temp);
+    println!("{:.1}°C is equal to {:.1}°F", celsius_temp, fahrenheit_temp);
+}
