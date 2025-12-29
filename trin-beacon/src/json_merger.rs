6914ba3ff8a9f45a1c4b7096302ec15abe63dbd1
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-29 20:22:47 +0000

    Add utility to merge multiple JSON files into a single JSON object

---
+++ trin-beacon/src/json_merger.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
+
+pub fn merge_json_files(file_paths: &[&str]) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+    let mut merged_map = HashMap::new();
+
+    for path_str in file_paths {
+        let path = Path::new(path_str);
+        if !path.exists() {
+            return Err(format!("File not found: {}", path_str).into());
+        }
+
+        let content = fs::read_to_string(path)?;
+        let json_value: serde_json::Value = serde_json::from_str(&content)?;
+
+        if let serde_json::Value::Object(map) = json_value {
+            for (key, value) in map {
+                merged_map.insert(key, value);
+            }
+        } else {
+            return Err("Each JSON file must contain a JSON object".into());
+        }
+    }
+
+    Ok(serde_json::Value::Object(merged_map.into_iter().collect()))
+}
