877395619e1529bc98675e4c9a6eb2376b1db060
InkspillVerse <inkspill.verse@outlook.com>
2025-12-31 16:32:27 +0000

    Implement a basic WebSocket echo server using tokio-tungstenite

---
+++ trin-execution/resources/genesis/websocket_server.rs
@@ -0,0 +1,41 @@
+use tokio::net::TcpListener;
+use tokio_tungstenite::accept_async;
+use futures_util::{SinkExt, StreamExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await?;
+    println!("WebSocket echo server listening on {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(async move {
+            let ws_stream = match accept_async(stream).await {
+                Ok(ws) => ws,
+                Err(e) => {
+                    eprintln!("Error during WebSocket handshake: {}", e);
+                    return;
+                }
+            };
+
+            let (mut write, mut read) = ws_stream.split();
+
+            while let Some(msg) = read.next().await {
+                match msg {
+                    Ok(message) => {
+                        if let Err(e) = write.send(message).await {
+                            eprintln!("Error sending message: {}", e);
+                            break;
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Error receiving message: {}", e);
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    Ok(())
+}
