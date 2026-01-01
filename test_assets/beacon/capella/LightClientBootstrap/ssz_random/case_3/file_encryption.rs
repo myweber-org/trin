3d84c0740279fed3fedad3dc64f46e6742158b17
InkspillVerse <inkspill.verse@outlook.com>
2026-01-01 22:26:17 +0000

    Add file encryption utility with AES-256-GCM

--- test_assets/beacon/capella/LightClientBootstrap/ssz_random/case_3/file_encryption.rs
+++ test_assets/beacon/capella/LightClientBootstrap/ssz_random/case_3/file_encryption.rs
@@ -273,4 +273,28 @@ mod tests {
         let decrypted_content = fs::read(decrypted_file.path()).unwrap();
         assert_eq!(original_content, decrypted_content.as_slice());
     }
+}use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce,
+};
+use std::error::Error;
+
+pub fn encrypt_file(data: &[u8], key: &[u8; 32]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key));
+    let nonce = Nonce::from_slice(&[0u8; 12]);
+    let ciphertext = cipher.encrypt(nonce, data)?;
+    Ok(ciphertext)
+}
+
+pub fn decrypt_file(ciphertext: &[u8], key: &[u8; 32]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key));
+    let nonce = Nonce::from_slice(&[0u8; 12]);
+    let plaintext = cipher.decrypt(nonce, ciphertext)?;
+    Ok(plaintext)
+}
+
+pub fn generate_key() -> [u8; 32] {
+    let mut key = [0u8; 32];
+    OsRng.fill_bytes(&mut key);
+    key
 }
