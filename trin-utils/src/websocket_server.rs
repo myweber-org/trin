81186a4e01e0e9e135afa4008099c3785c2fabf3
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-09 01:29:28 +0000

    Add a simple WebSocket echo server using tokio-tungstenite

---
+++ trin-utils/src/websocket_server.rs
@@ -0,0 +1,39 @@
+
+use futures_util::{SinkExt, StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::Message;
+use tokio_tungstenite::{accept_async, WebSocketStream};
+
+async fn handle_connection(stream: TcpStream) {
+    let ws_stream = accept_async(stream).await.expect("Failed to accept");
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(msg) = read.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                if let Err(e) = write.send(Message::Text(text)).await {
+                    eprintln!("Send error: {}", e);
+                    break;
+                }
+            }
+            Ok(Message::Close(_)) => {
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error reading message: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("WebSocket server listening on ws://127.0.0.1:8080");
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
+    }
+}
