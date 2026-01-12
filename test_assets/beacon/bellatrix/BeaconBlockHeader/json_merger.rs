4f007dc255275c14b997956e6d1e83ef75ea6ed3
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-12 01:10:54 +0000

    Add utility to merge multiple JSON files into a single JSON object

---
+++ test_assets/beacon/bellatrix/BeaconBlockHeader/json_merger.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::Path;
+
+pub fn merge_json_files(file_paths: &[&str]) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+    let mut merged_map = HashMap::new();
+
+    for path_str in file_paths {
+        let path = Path::new(path_str);
+        let file = File::open(path)?;
+        let mut reader = BufReader::new(file);
+        let mut contents = String::new();
+        reader.read_to_string(&mut contents)?;
+
+        let json_value: serde_json::Value = serde_json::from_str(&contents)?;
+
+        if let serde_json::Value::Object(map) = json_value {
+            for (key, value) in map {
+                merged_map.insert(key, value);
+            }
+        } else {
+            return Err("Each JSON file must contain a JSON object".into());
+        }
+    }
+
+    Ok(serde_json::Value::Object(merged_map.into_iter().collect()))
+}
