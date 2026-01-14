c9c022301841be66cb4693170256b9cb80e5383c
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-14 18:40:22 +0000

    Add file encryption utility with AES-256-GCM

---
+++ test_assets/beacon/deneb/ExecutionPayloadHeader/ssz_random/file_encryption.rs
@@ -0,0 +1,42 @@
+use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce,
+};
+use std::error::Error;
+
+pub fn encrypt_data(plaintext: &[u8]) -> Result<(Vec<u8>, Vec<u8>), Box<dyn Error>> {
+    let key = Aes256Gcm::generate_key(&mut OsRng);
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Nonce::from_slice(b"unique_nonce_");
+    
+    let ciphertext = cipher.encrypt(nonce, plaintext)
+        .map_err(|e| format!("Encryption failed: {}", e))?;
+    
+    Ok((ciphertext, key.to_vec()))
+}
+
+pub fn decrypt_data(ciphertext: &[u8], key: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let key = Key::<Aes256Gcm>::from_slice(key);
+    let cipher = Aes256Gcm::new(key);
+    let nonce = Nonce::from_slice(b"unique_nonce_");
+    
+    let plaintext = cipher.decrypt(nonce, ciphertext)
+        .map_err(|e| format!("Decryption failed: {}", e))?;
+    
+    Ok(plaintext)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encryption_roundtrip() {
+        let original_data = b"Secret message for encryption test";
+        
+        let (ciphertext, key) = encrypt_data(original_data).unwrap();
+        let decrypted_data = decrypt_data(&ciphertext, &key).unwrap();
+        
+        assert_eq!(original_data.to_vec(), decrypted_data);
+    }
+}
