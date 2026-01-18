2d826a30aa34573cec87f5dda0cbe1f31c80e4e2
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-18 00:28:12 +0000

    Add file encryption utility with AES-256-GCM

--- test_assets/beacon/bellatrix/LightClientFinalityUpdate/ssz_random/case_4/file_encryption.rs
+++ test_assets/beacon/bellatrix/LightClientFinalityUpdate/ssz_random/case_4/file_encryption.rs
@@ -69,3 +69,64 @@ mod tests {
         assert_eq!(test_data.to_vec(), result);
     }
 }
+use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce,
+};
+use std::fs;
+use std::io::{self, Write};
+use std::path::Path;
+
+pub struct FileEncryptor {
+    cipher: Aes256Gcm,
+}
+
+impl FileEncryptor {
+    pub fn new() -> Self {
+        let key = Aes256Gcm::generate_key(&mut OsRng);
+        let cipher = Aes256Gcm::new(&key);
+        Self { cipher }
+    }
+
+    pub fn encrypt_file(&self, input_path: &Path, output_path: &Path) -> io::Result<()> {
+        let data = fs::read(input_path)?;
+        let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+        
+        let encrypted_data = self.cipher
+            .encrypt(&nonce, data.as_ref())
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        
+        let mut output = fs::File::create(output_path)?;
+        output.write_all(&nonce)?;
+        output.write_all(&encrypted_data)?;
+        
+        Ok(())
+    }
+
+    pub fn decrypt_file(&self, input_path: &Path, output_path: &Path) -> io::Result<()> {
+        let encrypted_data = fs::read(input_path)?;
+        
+        if encrypted_data.len() < 12 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "File too short to contain nonce"
+            ));
+        }
+        
+        let (nonce_bytes, ciphertext) = encrypted_data.split_at(12);
+        let nonce = Nonce::from_slice(nonce_bytes);
+        
+        let decrypted_data = self.cipher
+            .decrypt(nonce, ciphertext)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        
+        fs::write(output_path, decrypted_data)?;
+        Ok(())
+    }
+}
+
+pub fn generate_key_file(path: &Path) -> io::Result<()> {
+    let key = Aes256Gcm::generate_key(&mut OsRng);
+    fs::write(path, key.as_slice())?;
+    Ok(())
+}
