65d23730343e782588a12d1216234da454ffba40
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-21 00:14:02 +0000

    Implement a basic WebSocket echo server using tokio-tungstenite

---
+++ test_assets/beacon/capella/ExecutionPayloadHeader/ssz_random/case_0/websocket_server.rs
@@ -0,0 +1,50 @@
+use futures_util::{SinkExt, StreamExt};
+use tokio::net::TcpListener;
+use tokio_tungstenite::accept_async;
+use tokio_tungstenite::tungstenite::Message;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await?;
+    println!("WebSocket echo server listening on {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(async move {
+            let ws_stream = match accept_async(stream).await {
+                Ok(ws) => ws,
+                Err(e) => {
+                    eprintln!("Error during WebSocket handshake: {}", e);
+                    return;
+                }
+            };
+
+            let (mut write, mut read) = ws_stream.split();
+
+            while let Some(msg) = read.next().await {
+                match msg {
+                    Ok(Message::Text(text)) => {
+                        println!("Received text message: {}", text);
+                        if let Err(e) = write.send(Message::Text(text)).await {
+                            eprintln!("Error sending message: {}", e);
+                            break;
+                        }
+                    }
+                    Ok(Message::Close(_)) => {
+                        println!("Client initiated close");
+                        break;
+                    }
+                    Ok(_) => {
+                        // Ignore other message types for this echo example
+                    }
+                    Err(e) => {
+                        eprintln!("Error reading message: {}", e);
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    Ok(())
+}
