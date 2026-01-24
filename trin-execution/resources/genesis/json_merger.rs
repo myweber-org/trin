44fe0654097675819b37f8b18c15f9cca6f7fb10
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-24 00:54:21 +0000

    Add utility to merge multiple JSON files into a single JSON object

--- trin-execution/resources/genesis/json_merger.rs
+++ trin-execution/resources/genesis/json_merger.rs
@@ -1,3 +1,4 @@
+
 use serde_json::{Map, Value};
 use std::fs;
 use std::path::Path;
@@ -7,6 +8,10 @@ pub fn merge_json_files(file_paths: &[&str]) -> Result<Value, Box<dyn std::error
 
     for path_str in file_paths {
         let path = Path::new(path_str);
+        if !path.exists() {
+            continue;
+        }
+
         let content = fs::read_to_string(path)?;
         let json_value: Value = serde_json::from_str(&content)?;
 
@@ -14,10 +19,14 @@ pub fn merge_json_files(file_paths: &[&str]) -> Result<Value, Box<dyn std::error
             for (key, value) in map {
                 merged_map.insert(key, value);
             }
-        } else {
-            return Err("Each JSON file must contain a JSON object".into());
         }
     }
 
     Ok(Value::Object(merged_map))
+}
+
+pub fn write_merged_json(output_path: &str, value: &Value) -> Result<(), Box<dyn std::error::Error>> {
+    let json_string = serde_json::to_string_pretty(value)?;
+    fs::write(output_path, json_string)?;
+    Ok(())
 }
