a92d91dac356a6609a3d19ac61bdd281c2694dda
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-27 14:29:03 +0000

    Add file encryption utility with AES-256-GCM and PBKDF2 key derivation

--- tests/utils/file_encryption.rs
+++ tests/utils/file_encryption.rs
@@ -69,3 +69,62 @@ mod tests {
         assert_eq!(test_data.to_vec(), decrypted);
     }
 }
+use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce
+};
+use pbkdf2::{pbkdf2_hmac, Params};
+use sha2::Sha256;
+use std::error::Error;
+
+const SALT_LENGTH: usize = 16;
+const NONCE_LENGTH: usize = 12;
+const PBKDF2_ITERATIONS: u32 = 100_000;
+
+pub struct EncryptedData {
+    pub ciphertext: Vec<u8>,
+    pub salt: [u8; SALT_LENGTH],
+    pub nonce: [u8; NONCE_LENGTH],
+}
+
+pub fn derive_key(password: &str, salt: &[u8]) -> Result<Key<Aes256Gcm>, Box<dyn Error>> {
+    let mut key = [0u8; 32];
+    let params = Params {
+        rounds: PBKDF2_ITERATIONS,
+        output_length: 32,
+    };
+    
+    pbkdf2_hmac::<Sha256>(password.as_bytes(), salt, params.rounds, &mut key)
+        .map_err(|e| e.into())
+        .map(|_| *Key::<Aes256Gcm>::from_slice(&key))
+}
+
+pub fn encrypt_data(plaintext: &[u8], password: &str) -> Result<EncryptedData, Box<dyn Error>> {
+    let mut salt = [0u8; SALT_LENGTH];
+    OsRng.fill_bytes(&mut salt);
+    
+    let mut nonce = [0u8; NONCE_LENGTH];
+    OsRng.fill_bytes(&mut nonce);
+    
+    let key = derive_key(password, &salt)?;
+    let cipher = Aes256Gcm::new(&key);
+    
+    let ciphertext = cipher
+        .encrypt(Nonce::from_slice(&nonce), plaintext)
+        .map_err(|e| e.to_string())?;
+    
+    Ok(EncryptedData {
+        ciphertext,
+        salt,
+        nonce,
+    })
+}
+
+pub fn decrypt_data(encrypted: &EncryptedData, password: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+    let key = derive_key(password, &encrypted.salt)?;
+    let cipher = Aes256Gcm::new(&key);
+    
+    cipher
+        .decrypt(Nonce::from_slice(&encrypted.nonce), encrypted.ciphertext.as_ref())
+        .map_err(|e| e.to_string().into())
+}
