0a4a955186ee471a42af5f01b847e9ff06a693aa
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-27 21:09:05 +0000

    Add a simple WebSocket echo server using tokio-tungstenite

---
+++ test_assets/beacon/capella/LightClientBootstrap/ssz_random/case_4/websocket_server.rs
@@ -0,0 +1,53 @@
+use std::net::SocketAddr;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::protocol::Message;
+use futures_util::{SinkExt, StreamExt};
+use tokio_tungstenite::accept_async;
+
+async fn handle_connection(stream: TcpStream, addr: SocketAddr) {
+    println!("New WebSocket connection from: {}", addr);
+    let ws_stream = match accept_async(stream).await {
+        Ok(ws) => ws,
+        Err(e) => {
+            eprintln!("Error during WebSocket handshake: {}", e);
+            return;
+        }
+    };
+
+    let (mut ws_sender, mut ws_receiver) = ws_stream.split();
+
+    while let Some(msg) = ws_receiver.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                println!("Received text message: {}", text);
+                let echo_msg = Message::Text(format!("Echo: {}", text));
+                if let Err(e) = ws_sender.send(echo_msg).await {
+                    eprintln!("Error sending message: {}", e);
+                    break;
+                }
+            }
+            Ok(Message::Close(_)) => {
+                println!("Client disconnected: {}", addr);
+                break;
+            }
+            Ok(_) => {
+                println!("Received non-text message from: {}", addr);
+            }
+            Err(e) => {
+                eprintln!("Error receiving message: {}", e);
+                break;
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("WebSocket echo server listening on: {}", addr);
+
+    while let Ok((stream, addr)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream, addr));
+    }
+}
