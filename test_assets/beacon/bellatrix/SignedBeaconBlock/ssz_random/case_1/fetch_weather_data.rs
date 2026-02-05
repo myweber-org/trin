84e7c6367d5d4ea2dc6833de6103a71eef012a91
heyQuim <hey.quim@hotmail.com>
2026-02-05 23:17:15 +0000

    Add function to fetch current weather data from OpenWeatherMap API

---
+++ test_assets/beacon/bellatrix/SignedBeaconBlock/ssz_random/case_1/fetch_weather_data.rs
@@ -0,0 +1,41 @@
+
+use reqwest;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct WeatherData {
+    main: Main,
+    name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Main {
+    temp: f64,
+    humidity: u8,
+}
+
+pub async fn get_weather(api_key: &str, city: &str) -> Result<WeatherData, reqwest::Error> {
+    let url = format!(
+        "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric",
+        city, api_key
+    );
+    
+    let response = reqwest::get(&url).await?;
+    let weather: WeatherData = response.json().await?;
+    
+    Ok(weather)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[tokio::test]
+    async fn test_weather_fetch() {
+        let api_key = "test_key";
+        let city = "London";
+        
+        let result = get_weather(api_key, city).await;
+        assert!(result.is_err());
+    }
+}
