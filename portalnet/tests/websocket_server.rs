683f8285a2219eb01b3c606b3e60e93952bd2bfd
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-11 14:38:52 +0000

    Implement a basic WebSocket echo server using tokio-tungstenite

---
+++ portalnet/tests/websocket_server.rs
@@ -0,0 +1,49 @@
+use futures_util::{SinkExt, StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::protocol::Message;
+use tokio_tungstenite::accept_async;
+
+async fn handle_connection(stream: TcpStream) {
+    let ws_stream = match accept_async(stream).await {
+        Ok(ws) => ws,
+        Err(e) => {
+            eprintln!("Error during WebSocket handshake: {}", e);
+            return;
+        }
+    };
+
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(msg) = read.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                println!("Received text message: {}", text);
+                let echo_msg = Message::Text(format!("Echo: {}", text));
+                if let Err(e) = write.send(echo_msg).await {
+                    eprintln!("Error sending message: {}", e);
+                    break;
+                }
+            }
+            Ok(Message::Close(_)) => {
+                println!("Client closed connection");
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error reading message: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("WebSocket echo server listening on {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
+    }
+}
