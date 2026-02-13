480579383f1afd047c34466e1d4cc5ae4a4a5177
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-13 20:31:42 +0000

    Add a simple WebSocket echo server using tokio-tungstenite

---
+++ test_assets/beacon/bellatrix/BeaconBlockHeader/ssz_random/case_1/websocket_server.rs
@@ -0,0 +1,47 @@
+use futures_util::{SinkExt, StreamExt};
+use tokio::net::TcpListener;
+use tokio_tungstenite::accept_async;
+use tokio_tungstenite::tungstenite::Message;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await?;
+    println!("WebSocket server listening on {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
+    }
+    Ok(())
+}
+
+async fn handle_connection(stream: tokio::net::TcpStream) {
+    let ws_stream = match accept_async(stream).await {
+        Ok(ws) => ws,
+        Err(e) => {
+            eprintln!("Error during WebSocket handshake: {}", e);
+            return;
+        }
+    };
+
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(msg) = read.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                if let Err(e) = write.send(Message::Text(text)).await {
+                    eprintln!("Error sending message: {}", e);
+                    break;
+                }
+            }
+            Ok(Message::Close(_)) => {
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error reading message: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
+}
