67c340e4c2e11a8593e210fbde4ac82784b4d088
FelixMakes <felix.makes@hotmail.com>
2026-02-14 14:25:29 +0000

    Add a simple WebSocket echo server example using tokio-tungstenite

---
+++ ethportal-api/src/dashboard/websocket_server.rs
@@ -0,0 +1,41 @@
+use futures_util::{SinkExt, StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::Message;
+use tokio_tungstenite::{accept_async, WebSocketStream};
+
+async fn handle_connection(stream: TcpStream) {
+    let ws_stream = accept_async(stream).await.expect("Failed to accept");
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(msg) = read.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                println!("Received text message: {}", text);
+                let echo_msg = Message::Text(format!("Echo: {}", text));
+                if let Err(e) = write.send(echo_msg).await {
+                    eprintln!("Error sending message: {}", e);
+                    break;
+                }
+            }
+            Ok(Message::Close(_)) => {
+                println!("Client disconnected");
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error processing message: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("WebSocket server listening on ws://127.0.0.1:8080");
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
+    }
+}
