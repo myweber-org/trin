55c48012fd4695a85625b553174139c789af4924
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-16 20:40:45 +0000

    Add temperature conversion utility with Celsius to Fahrenheit function

--- test_assets/beacon/capella/LightClientBootstrap/ssz_random/temperature_converter.rs
+++ test_assets/beacon/capella/LightClientBootstrap/ssz_random/temperature_converter.rs
@@ -33,3 +33,15 @@ mod tests {
         assert_eq!(fahrenheit_to_celsius(-40.0), -40.0);
     }
 }
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+    (celsius * 9.0 / 5.0) + 32.0
+}
+
+fn main() {
+    let celsius_temps = [0.0, 20.0, 37.0, 100.0];
+    
+    for &temp in &celsius_temps {
+        let fahrenheit = celsius_to_fahrenheit(temp);
+        println!("{:.1}°C = {:.1}°F", temp, fahrenheit);
+    }
+}
