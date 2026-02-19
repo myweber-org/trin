b547cdfc3d475044084210ca570d4e9e3296773c
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-19 23:54:03 +0000

    Add initial packet capture and analysis functionality using pnet

--- tests/network_packet_analyzer.rs
+++ tests/network_packet_analyzer.rs
@@ -184,4 +184,78 @@ fn main() {
 
     stats.display();
     println!("\nCapture complete. Analyzed {} packets.", packet_count);
+}use pnet::datalink::{self, Channel::Ethernet};
+use pnet::packet::ethernet::{EthernetPacket, EtherTypes};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::Packet;
+use std::env;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let interface_name = env::args().nth(1).unwrap_or_else(|| "eth0".to_string());
+    let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .find(|iface| iface.name == interface_name)
+        .expect("Interface not found");
+
+    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unsupported channel type"),
+        Err(e) => panic!("Failed to create channel: {}", e),
+    };
+
+    println!("Starting packet capture on interface: {}", interface_name);
+    let mut packet_count = 0;
+
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                packet_count += 1;
+                if let Some(eth_packet) = EthernetPacket::new(packet) {
+                    match eth_packet.get_ethertype() {
+                        EtherTypes::Ipv4 => {
+                            if let Some(ip_packet) = Ipv4Packet::new(eth_packet.payload()) {
+                                println!(
+                                    "Packet #{}: IPv4 | Src: {} -> Dst: {} | Protocol: {}",
+                                    packet_count,
+                                    ip_packet.get_source(),
+                                    ip_packet.get_destination(),
+                                    ip_packet.get_next_level_protocol()
+                                );
+
+                                if ip_packet.get_next_level_protocol() == pnet::packet::ip::IpNextHeaderProtocols::Tcp {
+                                    if let Some(tcp_packet) = TcpPacket::new(ip_packet.payload()) {
+                                        println!(
+                                            "  TCP | Src Port: {} -> Dst Port: {} | Flags: {:?}",
+                                            tcp_packet.get_source(),
+                                            tcp_packet.get_destination(),
+                                            tcp_packet.get_flags()
+                                        );
+                                    }
+                                }
+                            }
+                        }
+                        EtherTypes::Arp => {
+                            println!("Packet #{}: ARP packet detected", packet_count);
+                        }
+                        _ => {
+                            println!("Packet #{}: Other protocol: {:?}", packet_count, eth_packet.get_ethertype());
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Error receiving packet: {}", e);
+                break;
+            }
+        }
+
+        if packet_count >= 100 {
+            println!("Captured {} packets. Stopping.", packet_count);
+            break;
+        }
+    }
+
+    Ok(())
 }
