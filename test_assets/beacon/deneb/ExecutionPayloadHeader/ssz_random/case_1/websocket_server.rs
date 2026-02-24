29eedc308f535ef606682c157d9a3d702d66d59f
heyQuim <hey.quim@hotmail.com>
2026-02-24 22:48:51 +0000

    Implement a basic WebSocket server using tokio-tungstenite

---
+++ test_assets/beacon/deneb/ExecutionPayloadHeader/ssz_random/case_1/websocket_server.rs
@@ -0,0 +1,46 @@
+
+use futures_util::{SinkExt, StreamExt};
+use tokio::net::TcpListener;
+use tokio_tungstenite::accept_async;
+use tokio_tungstenite::tungstenite::Message;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await?;
+    println!("WebSocket server listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream));
+    }
+
+    Ok(())
+}
+
+async fn handle_connection(stream: tokio::net::TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    let ws_stream = accept_async(stream).await?;
+    let (mut write, mut read) = ws_stream.split();
+
+    while let Some(msg) = read.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                println!("Received text message: {}", text);
+                let response = format!("Echo: {}", text);
+                write.send(Message::Text(response)).await?;
+            }
+            Ok(Message::Close(_)) => {
+                println!("Client disconnected");
+                break;
+            }
+            Ok(_) => {
+                println!("Received non-text message, ignoring");
+            }
+            Err(e) => {
+                eprintln!("Error reading message: {}", e);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
