b539e6d98a7b15a4bdc900302790ddd9a00541e5
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-24 20:19:04 +0000

    Add a simple Celsius to Fahrenheit temperature converter function

--- test_assets/beacon/capella/SignedBeaconBlock/ssz_random/case_1/temperature_converter.rs
+++ test_assets/beacon/capella/SignedBeaconBlock/ssz_random/case_1/temperature_converter.rs
@@ -115,4 +115,12 @@ fn main() {
     }
 
     println!("Goodbye!");
+}fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+    (celsius * 9.0 / 5.0) + 32.0
+}
+
+fn main() {
+    let celsius_temp = 25.0;
+    let fahrenheit_temp = celsius_to_fahrenheit(celsius_temp);
+    println!("{}°C is equal to {}°F", celsius_temp, fahrenheit_temp);
 }
