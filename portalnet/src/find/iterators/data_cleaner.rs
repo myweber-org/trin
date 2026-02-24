5b5b893484bb31b312c3208e77f6b6ed38ab909c
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-24 19:08:59 +0000

    Add data cleaning utility to remove null values and duplicates

--- portalnet/src/find/iterators/data_cleaner.rs
+++ portalnet/src/find/iterators/data_cleaner.rs
@@ -37,3 +37,61 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Data cleaning completed successfully");
     Ok(())
 }
+use std::collections::HashSet;
+
+pub struct DataCleaner<T> {
+    data: Vec<T>,
+}
+
+impl<T> DataCleaner<T>
+where
+    T: Clone + Eq + std::hash::Hash,
+{
+    pub fn new(data: Vec<T>) -> Self {
+        DataCleaner { data }
+    }
+
+    pub fn remove_null_values(&mut self) -> &mut Self
+    where
+        T: Default + PartialEq,
+    {
+        let default_value = T::default();
+        self.data.retain(|item| *item != default_value);
+        self
+    }
+
+    pub fn remove_duplicates(&mut self) -> &mut Self {
+        let mut seen = HashSet::new();
+        self.data.retain(|item| seen.insert(item.clone()));
+        self
+    }
+
+    pub fn get_data(&self) -> &Vec<T> {
+        &self.data
+    }
+
+    pub fn into_data(self) -> Vec<T> {
+        self.data
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remove_duplicates() {
+        let data = vec![1, 2, 2, 3, 4, 4, 5];
+        let mut cleaner = DataCleaner::new(data);
+        cleaner.remove_duplicates();
+        assert_eq!(cleaner.get_data(), &vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_remove_null_values() {
+        let data = vec![0, 1, 0, 2, 3, 0];
+        let mut cleaner = DataCleaner::new(data);
+        cleaner.remove_null_values();
+        assert_eq!(cleaner.get_data(), &vec![1, 2, 3]);
+    }
+}
