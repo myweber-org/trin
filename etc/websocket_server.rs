74fc2985aa854e28f5ca727059befdbe0947a83b
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-05 19:15:31 +0000

    Implement a basic WebSocket server using tokio-tungstenite

--- etc/websocket_server.rs
+++ etc/websocket_server.rs
@@ -48,4 +48,49 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}use tokio::net::TcpListener;
+use tokio_tungstenite::accept_async;
+use futures_util::{SinkExt, StreamExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await?;
+    println!("WebSocket server listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(async move {
+            let ws_stream = match accept_async(stream).await {
+                Ok(ws) => ws,
+                Err(e) => {
+                    eprintln!("Error during WebSocket handshake: {}", e);
+                    return;
+                }
+            };
+
+            let (mut write, mut read) = ws_stream.split();
+
+            while let Some(message) = read.next().await {
+                match message {
+                    Ok(msg) => {
+                        if msg.is_text() || msg.is_binary() {
+                            let response = format!("Echo: {}", msg);
+                            if let Err(e) = write.send(response.into()).await {
+                                eprintln!("Error sending message: {}", e);
+                                break;
+                            }
+                        } else if msg.is_close() {
+                            break;
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Error reading message: {}", e);
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    Ok(())
 }
