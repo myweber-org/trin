e7f4b5f72ab7bd3d27547775ebd16ecfc7818b68
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-08 16:36:58 +0000

    Add utility to merge multiple JSON files into a single JSON array

--- light-client/src/consensus/rpc/json_merger.rs
+++ light-client/src/consensus/rpc/json_merger.rs
@@ -81,4 +81,91 @@ pub enum ConflictStrategy {
     Overwrite,
     Skip,
     MergeObjects,
+}use std::collections::HashMap;
+use std::fs::{self, File};
+use std::io::{BufReader, Read};
+use std::path::Path;
+
+type JsonValue = serde_json::Value;
+
+pub fn merge_json_files(file_paths: &[impl AsRef<Path>]) -> Result<JsonValue, Box<dyn std::error::Error>> {
+    let mut merged_array = Vec::new();
+
+    for path in file_paths {
+        let file = File::open(path.as_ref())?;
+        let mut reader = BufReader::new(file);
+        let mut content = String::new();
+        reader.read_to_string(&mut content)?;
+
+        let json_data: JsonValue = serde_json::from_str(&content)?;
+        
+        if let JsonValue::Array(arr) = json_data {
+            merged_array.extend(arr);
+        } else {
+            merged_array.push(json_data);
+        }
+    }
+
+    Ok(JsonValue::Array(merged_array))
+}
+
+pub fn merge_with_key_deduplication(
+    file_paths: &[impl AsRef<Path>],
+    key_field: &str,
+) -> Result<JsonValue, Box<dyn std::error::Error>> {
+    let mut unique_map: HashMap<String, JsonValue> = HashMap::new();
+
+    for path in file_paths {
+        let content = fs::read_to_string(path.as_ref())?;
+        let json_data: JsonValue = serde_json::from_str(&content)?;
+
+        let items = match json_data {
+            JsonValue::Array(arr) => arr,
+            _ => vec![json_data],
+        };
+
+        for item in items {
+            if let Some(obj) = item.as_object() {
+                if let Some(key_value) = obj.get(key_field) {
+                    if let Some(key_str) = key_value.as_str() {
+                        unique_map.insert(key_str.to_string(), item);
+                    }
+                }
+            }
+        }
+    }
+
+    let deduplicated: Vec<JsonValue> = unique_map.into_values().collect();
+    Ok(JsonValue::Array(deduplicated))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Write;
+    use tempfile::NamedTempFile;
+
+    fn create_temp_json(content: &str) -> NamedTempFile {
+        let mut file = NamedTempFile::new().unwrap();
+        write!(file, "{}", content).unwrap();
+        file
+    }
+
+    #[test]
+    fn test_merge_basic() {
+        let file1 = create_temp_json(r#"[{"id": 1}, {"id": 2}]"#);
+        let file2 = create_temp_json(r#"[{"id": 3}, {"id": 4}]"#);
+        
+        let result = merge_json_files(&[file1.path(), file2.path()]).unwrap();
+        assert_eq!(result.as_array().unwrap().len(), 4);
+    }
+
+    #[test]
+    fn test_deduplication() {
+        let file1 = create_temp_json(r#"[{"id": "a", "value": 1}]"#);
+        let file2 = create_temp_json(r#"[{"id": "a", "value": 2}]"#);
+        
+        let result = merge_with_key_deduplication(&[file1.path(), file2.path()], "id").unwrap();
+        assert_eq!(result.as_array().unwrap().len(), 1);
+    }
 }
